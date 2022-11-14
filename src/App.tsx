19e1ba7e830ea7c91544ef8051adf4a09a08146f
varbees <harshavar968@gmail.com>
2022-11-14 12:26:32 +0530

    Setting up react with vite, tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className='App'>
+      <div className='bg-black text-white'>Hello</div>
+    </div>
+  );
+}
+
+export default App;
